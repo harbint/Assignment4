b84508c177b2f0c3b7613a64ee3413ea4c50584c
harbint <travisharbin@gmail.com>
2020-03-21 21:30:23 -0500

    2

--- app/src/main/java/com/example/assignment4attempt1/MainActivity.java
+++ app/src/main/java/com/example/assignment4attempt1/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.assignment4attempt1;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //counter for number of history entries
+        final int[] counter = {-1};
+
         //associate variable EditTexts with their view components
         txtBalance = findViewById(R.id.txtBalance);
         txtDate = findViewById(R.id.txtDate);
@@ -45,7 +51,42 @@ public class MainActivity extends AppCompatActivity {
         final String formattedDate = df.format(c);
         txtDate.setText(formattedDate);
 
-        //Add button on click method
+        //pull strings from shared preferences
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        String balance = preferences.getString("Balance", "100");
+        String amount = preferences.getString("Amount", "");
+        String note = preferences.getString("Note", "");
+
+        //set strings to shared preferences
+        txtBalance.setText(balance);
+        txtAmount.setText(amount);
+        txtNote.setText(note);
+
+        //prep for history shared preference unloading
+        LayoutInflater ltInflater = getLayoutInflater();
+        LinearLayout subLayoutFields = findViewById(R.id.fieldHistory);
+        View view1 = ltInflater.inflate(R.layout.history, null, false);
+
+//        //add history views
+        EditText tempHistoryEditText;
+        tempHistoryEditText = view1.findViewById(R.id.txtString);
+        tempHistoryEditText.setText(preferences.getString("History" + String.valueOf(1), ""));
+        subLayoutFields.addView(view1);
+        //prep for history shared preference unloading
+        int loopcounter = preferences.getInt("Counter",0);
+        int i = 0;
+        while (loopcounter > i){
+            ltInflater = getLayoutInflater();
+            subLayoutFields = findViewById(R.id.fieldHistory);
+            view1 = ltInflater.inflate(R.layout.history, null, false);
+            EditText tempHistoryEditText1;
+            tempHistoryEditText1 = view1.findViewById(R.id.txtString);
+            tempHistoryEditText1.setText(preferences.getString("History" + String.valueOf(i), ""));
+            subLayoutFields.addView(view1);
+            i++;
+        }
+
+            //Add button on click method
         btnAdd.setOnClickListener(new View.OnClickListener() {
                                       @Override
                                       public void onClick(View v) {
@@ -66,6 +107,17 @@ public class MainActivity extends AppCompatActivity {
                                           DoneString = ("Added $" + txtAmount.getText().toString() + " on " + formattedDate + " from " + txtNote.getText().toString());
 
                                           myHistoryString.setText(DoneString);
+
+                                          SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                                          SharedPreferences.Editor editor = preferences.edit();
+                                          editor.putString("Balance",txtBalance.getText().toString());
+                                          editor.putString("Amount",txtAmount.getText().toString());
+                                          editor.putString("Note",txtNote.getText().toString());
+
+                                          counter[0] += 1;
+                                          editor.putString("History" + String.valueOf(counter[0]),DoneString);
+                                          editor.putInt("Counter",counter[0]);
+                                          editor.apply();
 
                                           subLayoutFields.addView(view1);
                                       }
@@ -93,9 +145,19 @@ public class MainActivity extends AppCompatActivity {
 
                                                myHistoryString.setText(DoneString);
 
+                                               SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                                               SharedPreferences.Editor editor = preferences.edit();
+                                               editor.putString("Balance",txtBalance.getText().toString());
+                                               editor.putString("Amount",txtAmount.getText().toString());
+                                               editor.putString("Note",txtNote.getText().toString());
+
+                                               counter[0] += 1;
+                                               editor.putString("History" + String.valueOf(counter[0]),DoneString);
+                                               editor.putInt("Counter",counter[0]);
+                                               editor.apply();
+
                                                subLayoutFields.addView(view1);
                                            }
                                        });
-
     }
 }
